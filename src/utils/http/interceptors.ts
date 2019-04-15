cf4676dc2ad4becb03974fa8d9f27f5574fddd60
jianzi0307 <jianzi0307@qq.com>
2019-04-16 02:05:54 +0800

    refactor(utils/http/interceptors.ts): 规范http拦截器错误状态码提示

--- src/utils/http/interceptors.ts
+++ src/utils/http/interceptors.ts
@@ -17,34 +17,40 @@ export default (http: any) => {
     },
     (error: any) => {
       if (!error['response']) {
+        console.log(error);
+        Vue.prototype.$Message.error(error.message);
         return Promise.reject(error);
       }
       switch (error.response.status) {
-        case 422:
-          const data = error.response.data.errors;
-          let content = '';
-          Object.keys(data).map((key) => {
-            const value = data[key];
-            content = value[0];
-          });
-          Vue.prototype.$Message.error(content);
+        // 请求参数有误
+        case 400:
+          Vue.prototype.$Message.error(error.response.data.msg || '请求参数有误');
           break;
-        case 403:
-          Vue.prototype.$Message.error(error.response.data.message || '您没有此操作权限！')
-          break;
+        // 未认证
         case 401:
           if (window.location.pathname !== '/auth/login') {
-            window.location.href = '/auth/login'
+            window.location.href = '/auth/login';
           }
           break;
-        case 404:
-          Vue.prototype.$Message.error('不存在的接口地址！')
+        // 无权限
+        case 403:
+          Vue.prototype.$Message.error(error.response.data.msg || '您没有此操作权限！');
           break;
+        // 不存在的接口
+        case 404:
+          Vue.prototype.$Message.error(error.response.data.msg || '不存在的接口地址！');
+          break;
+        case 422:
+          Vue.prototype.$Message.error(error.response.data.msg || 'Unprocessable Entity');
+          break;
+        // 服务器内部错误
         case 500:
+        // 未实现的服务
         case 501:
+        // 服务无效
         case 503:
         default:
-          Vue.prototype.$Message.error('服务器开小差了，请稍后再试~！')
+          Vue.prototype.$Message.error('服务器开小差了，请稍后再试~！');
           break;
       }
       return Promise.reject(error.response);
