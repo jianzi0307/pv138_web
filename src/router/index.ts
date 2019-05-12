643119400a19ab31f2a1ee0f1bd5a1638d7456e1
jianzi0307 <jianzi0307@qq.com>
2019-05-12 12:36:29 +0800

    test: 添加admin路由，测试layout布局

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { routes as auth } from '@/modules/auth';
 import { routes as web } from '@/modules/web';
+import { routes as admin } from '@/modules/admin';
 import Vue from 'vue'
 import Router from 'vue-router'
 import beforeEach from './beforeEach';
@@ -10,7 +11,7 @@ Vue.use(Router);
 const AppRoute = {
   path: '/',
   component: () => import('../App.vue'),
-  children: [...auth, ...web]
+  children: [...auth, ...web, ...admin]
 };
 
 const routes = [AppRoute];
